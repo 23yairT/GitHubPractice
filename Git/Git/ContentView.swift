ac9fa5358acfb2af73fb3081f667796a626a7d5b
PMStudent <PMStudent@RS-F251-M12.local>
2021-09-14 13:39:24 -0500

    hello, moon

---
+++ Git/Git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git
+//
+//  Created by PMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, moon!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git/Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by PMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
