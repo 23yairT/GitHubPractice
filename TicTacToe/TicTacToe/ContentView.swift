7090730b3715c61b8daa8d5041ddf9759a5ad9ac
23yairT <tyschool.23@gmail.com>
2021-09-15 14:19:30 -0500

    Update Tic Tac Toe
    Grid layout looks gooder now

--- TicTacToe/TicTacToe/ContentView.swift
+++ TicTacToe/TicTacToe/ContentView.swift
@@ -19,18 +19,45 @@ struct ContentView: View {
 }
 
 struct Home: View {
+    // Number of moves you can make
+    @State var moves: [String] = Array(repeating: "", count: 9)
+    // Identifiies the current player
+    @State var isPlaying = false
     var body: some View {
         VStack{
-          
             //Creates the Gridview
             LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 15) {
                 
                 ForEach(0..<9, id: \.self) { index in
                     
-                    Color.white
+                    ZStack {
+                        Color.white
+                        
+                        Text(moves[index])
+                            .font(.system(size: 55))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                    }
+                        .frame(width: getWidth(), height: getWidth())
+                        .cornerRadius(23)
+                        .onTapGesture (perform: {
+                            withAnimation(Animation.easeIn(duration: 0.01)) {
+                            
+                                moves[index] = isPlaying ? "X" : "O"
+                                isPlaying.toggle()
+                        }
+                    })
                 }
             }
+            
+            .padding(15)
         }
+    }
+    // Calculates width of squares
+    func getWidth() -> CGFloat {
+        let width = UIScreen.main.bounds.width - (30 + 30)
+        
+        return width / 3
     }
 }
 
