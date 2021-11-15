de67828510889c17a8ae242b3a16afed005a11b5
23yairT <tyschool.23@gmail.com>
2021-11-15 13:06:02 -0600

    Weather Forecast #1
    API is done

---
+++ TheBetterNewsOnSix/TheBetterNewsOnSix/Networking/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  TheBetterNewsOnSix
+//
+//  Created by PMStudent on 11/15/21.
+//
+
+import Foundation
+
+struct API {
+    static let key = "65cd818c52c97d2eaf17fb5299e269b0"
+}
---
+++ TheBetterNewsOnSix/TheBetterNewsOnSix/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheBetterNewsOnSix
+//
+//  Created by PMStudent on 11/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
